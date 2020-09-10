4dd873c0b56bb829d6b8b8916827f5f319dfb673
Sy Rashid <sy.rashid.11@gmail.com>
2020-09-10 23:23:24 +1000

    FEAT: Finished refresh with counter auto

---
+++ app/javascript/controllers/counter_controller.js
@@ -0,0 +1,26 @@
+import { Controller } from "stimulus";
+
+export default class extends Controller {
+  static targets = [ 'count' ];
+
+  connect() {
+    setInterval(this.refresh, 5000)
+  }
+
+  // refresh = () => {
+  //   console.log("5 secs have passed, and a fetch is about to happen!")
+  //   fetch('/teachers', { headers: { accept: "application/json" } })
+  //     .then(response => response.json())
+  //     .then((data) => {
+  //       this.countTarget.innerText = data.teachers.length;
+  //     });
+  // }
+
+
+  // Written with async await function (How we work with promises with newer JS)
+  refresh = async () => {
+    const response = await fetch('/teachers', { headers: { accept: "application/json" } });
+    const data = await response.json();
+    this.countTarget.innerText = data.teachers.length;
+  }
+}
